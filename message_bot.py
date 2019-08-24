ce86a228bef8c95e3e67b34c7ab3f5e56c0c854b
ksdfg <ksdfg123@gmail.com>
2019-08-24 18:48:46 +0530

    shifted to selenium, much less buggy now

--- message_bot.py
+++ message_bot.py
@@ -1,54 +1,59 @@
-import pyautogui as py
-import pandas as pd
 import time
 
-whatsapp_api = "https://api.whatsapp.com/send?phone=91"
-hello = "Bleep Bloop, I'm a bot\nHey, "
+import pandas as pd
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
 
+whatsapp_api = "https://api.whatsapp.com/send?phone=91"  # format of url to open chat with someone
+
+hello = "Beep Boop, I'm a bot\nHey, "  # what to print before name
+
+# what to print after name
 message = "This is to remind you that Ready Set Code is tomorrow at 3:30pm!" \
           " Location will be given out soon (Most probably in D-building, 3rd floor)." \
-          "\nSee you tomorrow! :)\n- The Script Group"
+          "\nSee you tomorrow! :)\n- The Script Group\n"
 
 
-def timer():
-    for x in range(5, 0, -1):
-        print(x)
-        time.sleep(1)
-
-
-def sendMessage(num, name):
-    api = whatsapp_api + str(num)
-    final = hello + name + "!\n" + message
+# method to send a message to someone
+def sendMessage(num, name, browser):
+    api = whatsapp_api + str(num)  # specific url
+    final = hello + name + "!\n" + message  # full message with name
     print(api, name)
-    py.hotkey('ctrl', 't')
-    time.sleep(1)
-    py.typewrite(api, interval=0.01)
-    py.press('enter')
-    time.sleep(3)
-    py.press(['\t', '\t', 'enter'])
-    while True:
-        if py.getWindowsWithTitle(') Whatsapp'):
-            break
-        time.sleep(0.2)
-    time.sleep(4)
-    py.typewrite(final, interval=0.01)
-    py.press('enter')
-    time.sleep(5)
-    py.hotkey('ctrl', 'w')
+    browser.get(api)  # open url in browser
+    browser.find_element_by_xpath('//*[@id="action-button"]').click()  # click on "send message" button
+
+    # wait till the text box is loaded onto the screen
+    try:
+        element_present = ec.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div[4]/div/footer'
+                                                                    '/div[1]/div[2]/div/div[2]'))
+        WebDriverWait(browser, 10000).until(element_present)
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+
+    # type out and send the full message
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]'
+    ).send_keys(final)
+
+    time.sleep(3)  # just so that we can supervise, otherwise it's too fast
 
 
 if __name__ == "__main__":
-    timer()
-    py.getWindowsWithTitle('Mozilla Firefox')[0].activate()
+    # read all entries to send message to
     excel = pd.read_excel("Tests.xlsx")
     numbers = excel['Number'].tolist()
     names = excel['Name'].tolist()
-    ctr = 0
 
+    # create a browser instance, login to whatsapp (one time per run)
+    webbrowser = webdriver.Firefox(executable_path='geckodriver.exe')
+    webbrowser.get('https://web.whatsapp.com/')
+    input('\npress enter when whatsapp login is done\n')
+
+    # send messages to all entries in file
     for num, name in zip(numbers, names):
-        sendMessage(num, name)
-        ctr += 1
-        if ctr == 30:
-            break
+        sendMessage(num, name, webbrowser)
 
-    py.hotkey('win', 'm')
+    webbrowser.close()  # close browser
