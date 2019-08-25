38531bea00bcdf2a2cc426639f92b4da97804017
ksdfg <ksdfg123@gmail.com>
2019-08-25 10:05:58 +0530

    waiting for important elements to load before performing action

--- message_bot.py
+++ message_bot.py
@@ -14,29 +14,31 @@ whatsapp_api = "https://api.whatsapp.com/send?phone=91"  # format of url to open
 message = "Hey, {} :wave:\nThis is to remind you that *Ready Set Code* is tomorrow at *3:30pm*! " \
           "Please report to _D building 2nd Floor_ with your QR code :smiley:" \
           "\nIf you have a laptop, and wish to use your own net, please report to _D401_ :sunglasses:" \
-          "\nSee you tomorrow! :v:\n- The Script Group bot 🤖\n"
+          "\nSee you tomorrow! :v:\n- SCRIPT bot 🤖\n"
+
+
+def waitTillLoaded(browser, element):
+    try:
+        element_present = ec.presence_of_element_located((By.XPATH, element))
+        WebDriverWait(browser, 10000).until(element_present)
+    except TimeoutException:
+        print("Timed out waiting for page to load")
 
 
 # method to send a message to someone
 def sendMessage(num, name, browser):
     api = whatsapp_api + str(num)  # specific url
-    final = emojize(message.format(name), use_aliases=True)  # full message with name
     print(api, name)
     browser.get(api)  # open url in browser
+
+    waitTillLoaded(browser, '//*[@id="action-button"]')  # wait till send message button is loaded
     browser.find_element_by_xpath('//*[@id="action-button"]').click()  # click on "send message" button
 
-    # wait till the text box is loaded onto the screen
-    try:
-        element_present = ec.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div[4]/div/footer'
-                                                                    '/div[1]/div[2]/div/div[2]'))
-        WebDriverWait(browser, 10000).until(element_present)
-    except TimeoutException:
-        print("Timed out waiting for page to load")
-
-    # type out and send the full message
+    # wait till the text box is loaded onto the screen, then type out and send the full message
+    waitTillLoaded(browser, '/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]')
     browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]'
-    ).send_keys(final)
+    ).send_keys(emojize(message.format(name), use_aliases=True))
 
     time.sleep(5)  # just so that we can supervise, otherwise it's too fast
 
@@ -50,7 +52,9 @@ if __name__ == "__main__":
     # create a browser instance, login to whatsapp (one time per run)
     webbrowser = webdriver.Firefox(executable_path='geckodriver.exe')
     webbrowser.get('https://web.whatsapp.com/')
-    input('\npress enter when whatsapp login is done\n')
+
+    # wait till the text box is loaded onto the screen
+    waitTillLoaded(webbrowser, '/html/body/div[1]/div/div/div[4]/div/div/div[1]')
 
     # send messages to all entries in file
     for num, name in zip(numbers, names):
