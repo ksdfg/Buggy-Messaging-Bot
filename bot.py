9a835cb77d26203a48fb095374dc611c645a2b32
ksdfg <ksdfg123@gmail.com>
2019-08-28 15:14:46 +0530

    bot sending qr to a chat id

--- bot.py
+++ bot.py
@@ -11,6 +11,14 @@ bot = telebot.TeleBot(data['bot-token'])
 @bot.message_handler(commands=['start'])
 def startMessage(message):
     bot.reply_to(message, 'hello ladiez')
+    with open(r"whatsapp-stuff\qr.png", 'rb') as qr:
+        bot.send_photo(data['chat-id'], qr)
+
+
+@bot.message_handler(commands=['stop'])
+def stopBot(message):
+    bot.stop_polling()
+    exit(0)
 
 
 bot.polling()
