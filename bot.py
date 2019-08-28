5751d99e91a06c6990a77882ae9a69a0e4e8f116
ksdfg <ksdfg123@gmail.com>
2019-08-28 15:22:02 +0530

    bot sending qr to a chat id on certain command

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import json
 
 import telebot
+import whatsapp_stuff.whatsapp as meow
 
-with open(r'whatsapp-stuff\data.json', 'r') as f:
+with open(r'whatsapp_stuff\data.json', 'r') as f:
     data = json.load(f)
 
 bot = telebot.TeleBot(data['bot-token'])
@@ -11,7 +12,7 @@ bot = telebot.TeleBot(data['bot-token'])
 @bot.message_handler(commands=['start'])
 def startMessage(message):
     bot.reply_to(message, 'hello ladiez')
-    with open(r"whatsapp-stuff\qr.png", 'rb') as qr:
+    with open(r'whatsapp_stuff\qr.png', 'rb') as qr:
         bot.send_photo(data['chat-id'], qr)
 
 
@@ -19,6 +20,15 @@ def startMessage(message):
 def stopBot(message):
     bot.stop_polling()
     exit(0)
+
+
+@bot.message_handler(commands=['whatsapp'])
+def startWhatsapp(message):
+    browser = meow.startSession()
+    with open(r'whatsapp_stuff\qr.png', 'rb') as qr:
+        bot.send_photo(data['chat-id'], qr)
+    input('meow')
+    browser.close()
 
 
 bot.polling()
